f164aa520ebd7713c14daaed2bbdcd8f5fd81cab
Nightly <5535krisi+github.com@gmail.com>
2024-12-14 13:35:59 +0200

    1.5 - Cleanup; Update to 1.21.4

--- src/main/java/me/pog5/vpnwarner/Vpnwarner.java
+++ src/main/java/me/pog5/vpnwarner/Vpnwarner.java
@@ -3,8 +3,8 @@ package me.pog5.vpnwarner;
 import net.fabricmc.api.ModInitializer;
 
 public class Vpnwarner implements ModInitializer {
-
     @Override
     public void onInitialize() {
+        // clientside mod; this is unused
     }
 }
